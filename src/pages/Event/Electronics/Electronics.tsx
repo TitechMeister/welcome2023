35bc62775fe7e67250358969ae0ed10c4e28a154
Suzuki Yuta <suzuki7yuta@gmail.com>
2023-03-31 20:41:28 +0900

    電装の作業体験の内容を追加

--- src/pages/Event/Electronics/Electronics.tsx
+++ src/pages/Event/Electronics/Electronics.tsx
@@ -1,19 +1,20 @@
 import * as React from 'react';
-import { ImageList, ImageListItem, Typography, Paper } from '@mui/material';
-import img_1 from './img/1.jpg'
+import { ImageList, ImageListItem, Typography, Paper, Button } from '@mui/material';
+import img_1 from './img/1.png'
 
 export default function Electronics() {
     return (
         <Paper sx={{ margin: 2 }} variant="outlined" square>
             <Typography variant='h5' padding={2}>作業体験(電装)</Typography>
-            <Typography padding={1}>
-                決まり次第お知らせします <br />
-            </Typography>
             <ImageList>
                 <ImageListItem key="1">
                     <img src={img_1} />
                 </ImageListItem>
             </ImageList>
+            <Typography padding={1}>
+                作業体験（電装）特設サイトを開きました。
+                <Button variant='outlined' href='https://titechmeister.github.io/Device-ATmega88_Board/'>詳しくはこちら</Button>
+            </Typography>
         </Paper>
     )
 };
